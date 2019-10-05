ec7a393f05c907d393bef8823ba7db256e8bdb86
alfonsovega97 <alfonsovega1005@hotmail.com>
2019-10-05 20:35:14 +0200

    BubbleSort

---
+++ PresentacionPEL7Octubre/Bubblesort/cmake-build-debug/CMakeFiles/Bubblesort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Bubblesort.exe"
+  "Bubblesort.exe.manifest"
+  "Bubblesort.pdb"
+  "CMakeFiles/Bubblesort.dir/main.cpp.obj"
+  "libBubblesort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bubblesort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
